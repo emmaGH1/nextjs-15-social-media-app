a4494f9d78552b76649326a37278aa798e800981
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-09-19 23:53:42 +0100

    feat: Add Toggle Visibility feature

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { Input, InputProps } from "./ui/input"
 import { cn } from "@/lib/utils"
+import { Eye, EyeOff } from "lucide-react"
 
 const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
     ({ className, type, ...props}, ref) => {
@@ -14,6 +15,19 @@ const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
                 ref={ref}
                 {...props}
                />
+               <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                title={showPassword ? "Hide password" : "Show password"}
+                className="absolute right-3 top-1/2 -translate-y-1/2 transform text-muted-foreground"
+               >
+                   {showPassword ? (
+                      <EyeOff className="size-5" />
+                   ) : (
+                      <Eye className="size-5" />
+                   )
+                }
+               </button>
             </div>
         )
     }
