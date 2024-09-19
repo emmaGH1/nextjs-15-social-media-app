3da92c3d10314eb94bf64048df20b6e2ce70e885
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-09-19 23:08:34 +0100

    feat: Implement initial functionality for SignUpForm

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -5,11 +5,12 @@ const requiredString = z.string().trim().min(1, "Required")
 export const signUpSchema = z.object({
     email: requiredString.email("Invalid email address"),
     username: requiredString.regex(
-       /^[a-zA-Z0-9._-]{3,20}$/, "Only letters, numbers, - and _ allowed",
+      /^[a-zA-Z0-9._-]{3,20}$/,
+      "Username must be 3-20 characters and can only contain letters, numbers, underscores (_), hyphens (-), and periods (.)"
     ),
     password: requiredString.min(8, "Must be at least 8 characters"),
-});
-
+  });
+  
 export type SignUpValues = z.infer<typeof signUpSchema>;
 
 export const loginSchema = z.object({
